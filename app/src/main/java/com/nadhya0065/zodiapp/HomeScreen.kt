da3b1644f7231ca65b4be09d79d0462672053b90
NadhyaSigalingging <nadhyasigalingging@gmail.com>
2025-04-05 23:03:55 +0700

    Membuat halaman Home

---
+++ app/src/main/java/com/nadhya0065/zodiapp/HomeScreen.kt
@@ -0,0 +1,67 @@
+package com.nadhya0065.zodiapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate("about") }) {
+                        Icon(
+                            imageVector = Icons.Default.Info,
+                            contentDescription = stringResource(id = R.string.about_app)
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(id = R.string.app_name),
+                modifier = Modifier.size(120.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = { navController.navigate("zodiac") }) {
+                Text(text = stringResource(id = R.string.start))
+            }
+        }
+    }
+}
--- app/src/main/java/com/nadhya0065/zodiapp/navigation/NavGraph.kt
+++ app/src/main/java/com/nadhya0065/zodiapp/navigation/NavGraph.kt
@@ -2,12 +2,11 @@ package com.nadhya0065.zodiapp.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.nadhya0065.zodiapp.AboutScreen
-import com.nadhya0065.zodiapp.HomeScreen
-import com.nadhya0065.zodiapp.ResultScreen
-import com.nadhya0065.zodiapp.ZodiacScreen
+import androidx.navigation.navArgument
+import com.nadhya0065.zodiapp.*
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -21,7 +20,16 @@ fun NavGraph(navController: NavHostController) {
         composable(Screen.Zodiac.route) {
             ZodiacScreen(navController)
         }
-        composable(Screen.Result.route) { backStackEntry ->
+        composable(
+            route = "result?name={name}&gender={gender}&day={day}&month={month}&year={year}",
+            arguments = listOf(
+                navArgument("name") { type = NavType.StringType; defaultValue = "" },
+                navArgument("gender") { type = NavType.StringType; defaultValue = "" },
+                navArgument("day") { type = NavType.StringType; defaultValue = "" },
+                navArgument("month") { type = NavType.StringType; defaultValue = "" },
+                navArgument("year") { type = NavType.StringType; defaultValue = "" },
+            )
+        ) { backStackEntry ->
             val name = backStackEntry.arguments?.getString("name") ?: ""
             val gender = backStackEntry.arguments?.getString("gender") ?: ""
             val day = backStackEntry.arguments?.getString("day") ?: ""
@@ -37,8 +45,6 @@ fun NavGraph(navController: NavHostController) {
                 navController = navController
             )
         }
-
-
         composable(Screen.About.route) {
             AboutScreen(navController)
         }
